de963d2d035caedf248c468630cb238aaebd4091
Larisa <kosareva.larisa@gmail.com>
2024-05-24 19:29:40 +0200

    challenge1

---
+++ painting/src/test/java/com/lts/painting/PaintingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lts.painting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
