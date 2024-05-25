dc8baa81d44f8634b455e4000e7da65ef66d5f46
Larisa <kosareva.larisa@gmail.com>
2024-05-25 18:55:24 +0200

    add ModelAndView to a request handler

--- shows-workbook/src/main/java/com/lts/shows_workbook/ShowsController.java
+++ shows-workbook/src/main/java/com/lts/shows_workbook/ShowsController.java
@@ -1,8 +1,12 @@
 package com.lts.shows_workbook;
 
+import java.util.Map;
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+// import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 
@@ -11,12 +15,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ShowsController {
 
     @GetMapping("/")
-    public String getShows(Model model){
-        model.addAttribute("show1", new Shows("Breaking Bad", "Ozymandias", 10.0));
-        model.addAttribute("show2", new Shows("Attack on Titan", "Hero", 9.9));
-        model.addAttribute("show3", new Shows("Attack on Titan", "Perfect Game", 9.9));
-        model.addAttribute("show4", new Shows("Star Wars: The Clone Wars", "Victory and Death", 9.9));
-        model.addAttribute("show5", new Shows("Mr. Robot", "407 Proxy Authentication Required", 9.9));
-        return "shows";
+    public ModelAndView getShows(){
+        Map<String, Shows> model = new HashMap<String, Shows>();
+        model.put("first", new Shows("Breaking Bad", "Ozymandias", 10.0));
+        model.put("second", new Shows("Attack on Titan", "Hero", 9.9));
+        model.put("third", new Shows("Attack on Titan", "Perfect Game", 9.9));
+        model.put("fourth", new Shows("Star Wars: The Clone Wars", "Victory and Death", 	9.9));
+        model.put("fifth", new Shows("Mr. Robot", "407 Proxy Authentication Required", 9.9));
+
+        return new ModelAndView("shows", model);
     }
 }
