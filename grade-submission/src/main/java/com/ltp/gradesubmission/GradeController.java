94ed9485837b5dbf2d878ca5f710cebc7aab6b4f
Larisa <kosareva.larisa@gmail.com>
2024-05-27 13:33:36 +0200

    execute a th-loop on an array

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,6 +1,7 @@
 package com.ltp.gradesubmission;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -11,13 +12,14 @@ import org.springframework.ui.Model;
 @Controller
 public class GradeController {
 
-    List<Grades> studentGrades = new ArrayList<>();
+    List<Grades> studentGrades = Arrays.asList(
+        new Grades( "Harry", "Potions", "C-"),
+        new Grades( "Hermione", "Arithmancy", "A+"),
+        new Grades( "Neville", "Charms", "A-")
+    );
 
     @GetMapping("/grades")
     public String getGrades(Model model){
-        studentGrades.add(new Grades( "Harry", "Potions", "C-"));
-        studentGrades.add(new Grades( "Hermione", "Arithmancy", "A+"));
-        studentGrades.add(new Grades( "Neville", "Charms", "-C"));
         
         model.addAttribute("grades", studentGrades);
         return "grades";
