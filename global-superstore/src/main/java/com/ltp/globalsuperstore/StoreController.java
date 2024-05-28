c1feec6ad3dead4da65bd6669cb627506dd04e76
Larisa <kosareva.larisa@gmail.com>
2024-05-28 14:35:03 +0200

    add form submit

--- global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -1,15 +1,22 @@
 package com.ltp.globalsuperstore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StoreController {
+
+    private List<Item> items = new ArrayList<Item>();
     
     @GetMapping("/")
     public String getForm(Model model) {
         model.addAttribute("categories", Constants.CATEGORIES);
+        model.addAttribute("item", new Item());
         return "form";
     }
 
@@ -17,5 +24,13 @@ public class StoreController {
     public String getInventory() {
         return "inventory";
     }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(Item item) {
+        items.add(item);
+        return "redirect:/inventory";
+
+    }
+
 
 }
