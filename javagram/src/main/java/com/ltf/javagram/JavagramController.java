5efae0067b531db01d09991c87ddadae349f43c4
Larisa <kosareva.larisa@gmail.com>
2024-05-30 19:46:20 +0200

    add handleSubmit function

--- javagram/src/main/java/com/ltf/javagram/JavagramController.java
+++ javagram/src/main/java/com/ltf/javagram/JavagramController.java
@@ -3,12 +3,16 @@ package com.ltf.javagram;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 @Controller
 public class JavagramController {
-    
+
     @GetMapping("/")
     public String getForm(Model model) {
+        model.addAttribute("user", new User());
         return "sign-up";
     }
 
@@ -16,6 +20,12 @@ public class JavagramController {
     public String getResult() {
         return "result";
     }
+
+    @PostMapping("/submitItem")
+    public String handleSubmit(User user) {
+        return "redirect:/result";
+    }
+    
 
 
 }
