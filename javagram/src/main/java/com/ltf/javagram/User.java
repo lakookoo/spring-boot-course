31e0095a6aa966a183cf07179e676658994c9c57
Larisa <kosareva.larisa@gmail.com>
2024-05-30 19:08:41 +0200

    add user class

---
+++ javagram/src/main/java/com/ltf/javagram/User.java
@@ -0,0 +1,65 @@
+package com.ltf.javagram;
+
+import java.util.Date;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private Date dateOfBirth;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String userName, String email, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+ 
+}
