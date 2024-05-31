04d10f54db74ecbf599ebf5646ced76594282d8f
Larisa <kosareva.larisa@gmail.com>
2024-05-31 13:40:05 +0200

    add cross field validation

--- javagram/src/main/java/com/ltf/javagram/JavagramController.java
+++ javagram/src/main/java/com/ltf/javagram/JavagramController.java
@@ -28,7 +28,11 @@ public class JavagramController {
 
     @PostMapping("/submitItem")
     public String handleSubmit(@Valid @ModelAttribute("user") User user, BindingResult result) {
+        if (user.getLastName().equals(user.getFirstName())){
+            result.rejectValue("lastName", "", "please enter valid data");
+        }
         if (result.hasErrors()) return "sign-up";
+        
 
         return "redirect:/result";
     }
