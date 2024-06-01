fa0b63c621ab0c733e7f30b131c25fd3c8669703
Larisa <kosareva.larisa@gmail.com>
2024-06-01 16:25:10 +0200

    add integration test

--- grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import com.ltp.gradesubmission.Grades;
 
-
+@Repository
 public class GradeRepository{
    private List<Grades> studentGrades = new ArrayList<>();
 
--- grade-submission/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
+++ grade-submission/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
@@ -1,13 +1,25 @@
 package com.ltp.gradesubmission;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ltp.gradesubmission.controller.GradeController;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GradeSubmissionApplicationTests {
+
+	@Autowired
+	private MockMvc mocMvc;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(mocMvc);
 	}
 
 }
