8dc92dafa686274db10b4a3c8cf58aa2d86fc21f
Larisa <kosareva.larisa@gmail.com>
2024-06-02 20:04:11 +0200

    add integration test

--- grade-submission/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
+++ grade-submission/src/test/java/com/ltp/gradesubmission/GradeSubmissionApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.ltp.gradesubmission.controller.GradeController;
 
@@ -15,11 +17,17 @@ import com.ltp.gradesubmission.controller.GradeController;
 class GradeSubmissionApplicationTests {
 
 	@Autowired
-	private MockMvc mocMvc;
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
-		assertNotNull(mocMvc);
+		assertNotNull(mockMvc);
 	}
+
+	@Test
+	public void testShowGradeFrom(){
+		RequestBuilder request = MockMvcRequestBuilders.get("/?=123");
+		mockMvc.perform(request);
+	} 
 
 }
