22a217b5e10949ac4d571f15cc039fe9d7f04417
Larisa <kosareva.larisa@gmail.com>
2024-06-05 20:35:53 +0200

    return json object

--- contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
+++ contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
@@ -1,6 +1,7 @@
 package com.ltp.contacts.repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,11 @@ import com.ltp.contacts.pojo.Contact;
 @Repository
 public class ContactRepository {
     
-    private List<Contact> contacts = new ArrayList<>();
+    private List<Contact> contacts = Arrays.asList(
+        new Contact("123", "John Snow", "1234567890"),
+        new Contact("456", "Tyrion Lanister", "0987654321"),
+        new Contact("789", "Hound", "1234554321")
+    );
 
     public List<Contact> getContacts() {
         return contacts;
--- contacts/src/main/java/com/ltp/contacts/web/ContactController.java
+++ contacts/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -3,12 +3,24 @@ package com.ltp.contacts.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class ContactController {
     
     @Autowired
     private ContactService contactService;
+
+    @GetMapping("/contact/{id}")
+    @ResponseBody
+    public Contact getContact(@PathVariable String id) {
+        return new Contact("123", "John Snow", "1234567890");
+    }
+    
 
 }
