c6b00c895b4e1a281d03ab2c9ee2633e273da55e
Larisa <kosareva.larisa@gmail.com>
2024-06-05 20:38:14 +0200

    use rest controller annotation

--- contacts/src/main/java/com/ltp/contacts/web/ContactController.java
+++ contacts/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -1,16 +1,17 @@
 package com.ltp.contacts.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class ContactController {
     
     @Autowired
