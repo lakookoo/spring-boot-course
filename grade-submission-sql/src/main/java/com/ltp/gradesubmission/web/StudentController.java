9e6e8e2a4d5d5dd1936977038b4a20978244253b
Larisa <kosareva.larisa@gmail.com>
2024-06-07 20:54:01 +0200

    get all students

--- grade-submission-sql/src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ grade-submission-sql/src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -46,7 +46,7 @@ public class StudentController {
 
     @GetMapping("/all")
     public ResponseEntity<List<Student>> getStudents() {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(studentService.getStudents(), HttpStatus.OK);
     }
     
     
