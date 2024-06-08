637c86d6b540e1606691a2ef98d53cbd9d7e8d00
Larisa <kosareva.larisa@gmail.com>
2024-06-08 21:18:01 +0200

    Add many to one relationship to crade table

--- grade-submission-sql/src/main/java/com/ltp/gradesubmission/entity/Grade.java
+++ grade-submission-sql/src/main/java/com/ltp/gradesubmission/entity/Grade.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -23,5 +25,9 @@ public class Grade {
     private Long id;
     @Column(name = "score", nullable = false)
     private String score;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", referencedColumnName = "id")
+    private Student student;
 
 }
