faf86f385e9ce2acf040c351d0a7050510afd880
Larisa <kosareva.larisa@gmail.com>
2024-06-09 19:13:30 +0200

    add biderectional OneToMany relationship

--- grade-submission-sql/src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ grade-submission-sql/src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -1,33 +1,43 @@
 package com.ltp.gradesubmission.entity;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
 
 @Entity
 @Table(name = "students")
 @Getter
 @Setter
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @NonNull
     @Column(name = "name", nullable = false)
     private String name;
+
+    @NonNull
     @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "student")
+    private List<Grade> grades;
 
     
 }
