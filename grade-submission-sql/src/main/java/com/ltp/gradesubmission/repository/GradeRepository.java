81aa6d96a6e901546b539a9cb3f542e939fd9568
Larisa <kosareva.larisa@gmail.com>
2024-06-11 14:21:09 +0200

    update Grade

--- grade-submission-sql/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
+++ grade-submission-sql/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -1,9 +1,12 @@
 package com.ltp.gradesubmission.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.ltp.gradesubmission.entity.Grade;
 
 public interface GradeRepository extends CrudRepository<Grade, Long> {
     Grade findByStudentId(Long studentId);
+    Grade findByStudentIdAndCourseId(Long studentId, Long courseId);
 }
