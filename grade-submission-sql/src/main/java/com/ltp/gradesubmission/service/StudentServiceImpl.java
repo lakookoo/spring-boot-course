7a482a864dc32e1192ea9630595427a42af22c56
Larisa <kosareva.larisa@gmail.com>
2024-06-12 12:38:20 +0200

    refactor serviceImplementations for all pojos

--- grade-submission-sql/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
+++ grade-submission-sql/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
@@ -20,11 +20,7 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public Student getStudent(Long id) {
         Optional<Student> student = studentRepository.findById(id);
-        if (student.isPresent()) {
-            return student.get();
-        } else {
-            throw new StudentNotFoundException(id);
-        }
+        return unwrapStudent(student, id);
         
     }
 
@@ -41,6 +37,11 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public List<Student> getStudents() {
         return (List<Student>)studentRepository.findAll();
+    }
+
+    static Student unwrapStudent(Optional<Student> entity, Long id) {
+        if (entity.isPresent()) return entity.get();
+        else throw new StudentNotFoundException(id);
     }
 
 
