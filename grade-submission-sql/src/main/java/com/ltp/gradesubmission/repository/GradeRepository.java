f998601122c4598f5b0d0ed90178729215b31bca
Larisa <kosareva.larisa@gmail.com>
2024-06-12 12:22:51 +0200

    add grade cruds

--- grade-submission-sql/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
+++ grade-submission-sql/src/main/java/com/ltp/gradesubmission/repository/GradeRepository.java
@@ -4,8 +4,18 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
+
 import com.ltp.gradesubmission.entity.Grade;
+
+import jakarta.transaction.Transactional;
+import java.util.List;
+
 
 public interface GradeRepository extends CrudRepository<Grade, Long> {
     Optional<Grade> findByStudentIdAndCourseId(Long studentId, Long courseId);
+    @Transactional
+    void deleteByStudentIdAndCourseId(Long studentId, Long courseId);
+    List<Grade> findByStudentId(Long studentId);
+    List<Grade> findByCourseId(Long courseId);
+
 }
--- grade-submission-sql/src/main/java/com/ltp/gradesubmission/web/GradeController.java
+++ grade-submission-sql/src/main/java/com/ltp/gradesubmission/web/GradeController.java
@@ -45,22 +45,23 @@ public class GradeController {
 
     @DeleteMapping("/student/{studentId}/course/{courseId}")
     public ResponseEntity<HttpStatus> deleteGrade(@PathVariable Long studentId, @PathVariable Long courseId){
+        gradeService.deleteGrade(studentId, courseId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/student/{studentId}")
     public ResponseEntity<List<Grade>> getStudentGrades(@PathVariable Long studentId) {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(gradeService.getStudentGrades(studentId) ,HttpStatus.OK);
     }
 
     @GetMapping("/course/{courseId}")
     public ResponseEntity<List<Grade>> getCourseGrades(@PathVariable Long coursetId) {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(gradeService.getCourseGrades(coursetId), HttpStatus.OK);
     }
 
     @GetMapping("/all")
     public ResponseEntity<List<Grade>> getGrades() {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(gradeService.getAllGrades(), HttpStatus.OK);
     }
 
 }
