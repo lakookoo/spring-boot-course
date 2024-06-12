4230bf9bb4d8f840ea940e1223fdc96a49532440
Larisa <kosareva.larisa@gmail.com>
2024-06-12 18:00:00 +0200

    update Student service

--- grade-submission-sql/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
+++ grade-submission-sql/src/main/java/com/ltp/gradesubmission/service/StudentServiceImpl.java
@@ -2,9 +2,11 @@ package com.ltp.gradesubmission.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import com.ltp.gradesubmission.entity.Course;
 import com.ltp.gradesubmission.entity.Student;
 import com.ltp.gradesubmission.exception.StudentNotFoundException;
 import com.ltp.gradesubmission.repository.StudentRepository;
@@ -37,6 +39,12 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public List<Student> getStudents() {
         return (List<Student>)studentRepository.findAll();
+    }
+
+    @Override
+    public Set<Course> getEnrolledCourses(Long id) {
+        Student student = getStudent(id);
+        return student.getCourses();
     }
 
     static Student unwrapStudent(Optional<Student> entity, Long id) {
