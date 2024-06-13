ff353f58db0bfb921a00414ff404ce64cd0428b8
Larisa <kosareva.larisa@gmail.com>
2024-06-13 19:39:45 +0200

    add user details and statless authentication

--- basic-auth-contacts/src/main/java/com/ltp/contacts/security/SecurityConfig.java
+++ basic-auth-contacts/src/main/java/com/ltp/contacts/security/SecurityConfig.java
@@ -3,20 +3,55 @@ package com.ltp.contacts.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.AllArgsConstructor;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authz -> authz
-                                .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+            .authorizeHttpRequests(authz -> authz
+                .anyRequest().authenticated()
+            )
+            .httpBasic(withDefaults());
+        
+        http
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users(){
+        UserDetails admin = User.builder()
+            .username("admin")
+            .password(passwordEncoder.encode("admin-pass"))
+            .roles("ADMIN")
+            .build();
+
+            UserDetails user = User.builder()
+            .username("user")
+            .password(passwordEncoder.encode("user-pass"))
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
     
 }
