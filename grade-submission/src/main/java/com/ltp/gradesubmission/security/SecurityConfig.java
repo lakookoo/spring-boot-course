6e6f30396579b6353d372c0a0e31827adb259baf
Larisa <kosareva.larisa@gmail.com>
2024-06-20 19:34:21 +0200

    add request matcher for a register path

--- grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.ltp.gradesubmission.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import lombok.AllArgsConstructor;
@@ -23,6 +24,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/h2/**").permitAll()
+                .requestMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll() 
                 .anyRequest().authenticated()
             )
             .sessionManagement(sessionManagement -> sessionManagement
